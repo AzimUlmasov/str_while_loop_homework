84dcbe7cbf1d8f1dd2c44114720393889ff740b8
AzimUlmasov <azimulmasov2@gmail.com>
2023-01-10 09:37:38 +0500

    04

--- While04.py
+++ While04.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    idx = 0
+    count = 0
+    while idx < len(s):
+        if s[idx].isupper():
+            count += 1
+        idx += 1 
+    return count
+print(main('CodeschoolUz'))
