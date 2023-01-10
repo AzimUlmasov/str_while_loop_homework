676df70a8ef819224037a93eb95e9b86f5683553
AzimUlmasov <azimulmasov2@gmail.com>
2023-01-10 11:50:55 +0500

    06

--- While06.py
+++ While06.py
@@ -7,3 +7,11 @@ def main(s):
     Returns:
         int: return answer
     """
+    vowels = 'aeiou'
+    a, x = 0, 0
+    while a < len(s):
+        if not s[a].lower() in vowels and s[a].isalpha():
+            x += 1
+        a += 1 
+    return x
+print(main('CodeschoolUz'))
