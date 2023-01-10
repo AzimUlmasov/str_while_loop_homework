6c04707b0deb000b1bf80bdfa857007c655d898e
AzimUlmasov <azimulmasov2@gmail.com>
2023-01-10 11:57:05 +0500

    07

--- While07.py
+++ While07.py
@@ -5,5 +5,13 @@ def main(s):
         s: str
     Returns:
         int: return answer
+
     """
-    return
+    idx = 0
+    count = 0
+    while idx < len(s):
+        if s[idx].isdigit() and int(s[idx])%2==0:
+            count += 1
+        idx += 1 
+    return count
+print(main('56786543250'))
